de67c08fd7a83c0066f251dba25513a4224e0b82
David Shaw <david.shaw1242@gmail.com>
2024-09-14 08:45:43 -0400

    added a player group

--- main.py
+++ main.py
@@ -10,6 +10,9 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     running = True
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
 
@@ -19,10 +22,17 @@ def main():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
+                
+                
+        for thing in updatable:
+            thing.update(dt)
+
         # fill the screen with a color to wipe away previous frame
         screen.fill("black")
-        player.draw(screen)
-        player.update(dt)
+        
+        
+        for item in drawable:
+            item.draw(screen)
         pygame.display.flip()
         
 
