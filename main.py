06acf696478bf2c2ad438d24e0a53391b7c62257
David Shaw <david.shaw1242@gmail.com>
2024-09-14 08:04:04 -0400

    adjusted the x and y calculation in player class

--- main.py
+++ main.py
@@ -10,10 +10,8 @@ def main():
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
     running = True
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     dt = 0
-    x = SCREEN_WIDTH / 2
-    y = SCREEN_HEIGHT / 2
-    player = Player(x, y)
 
     # infinite loop for now for the game
     while running:
