48934384b0f0f78ddfbe3b28095d2680743b3cae
David Shaw <dshaw11@utk.edu>
2024-09-16 08:34:34 -0400

    started setting asteroids class

---
+++ asteroids.py
@@ -0,0 +1,9 @@
+from circleshape import CircleShape
+
+class Asteroid(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self):
+        pass
+
