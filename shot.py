da996d1d9e5267ae8be8ee972f01f3de56a168fb
David Shaw <dshaw11@utk.edu>
2024-09-16 15:10:08 -0400

    started to add shot class

---
+++ shot.py
@@ -0,0 +1,9 @@
+from circleshape import *
+
+
+class Shot(CircleShape):
+    def __init__(self, x, y, radius):
+        super().__init__(x, y, radius)
+
+    def draw(self, screen):
+        pygame.draw.circle(screen, "white", self.position, self.radius, width=2)
