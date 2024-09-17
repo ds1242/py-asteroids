b8a39c885491a8c3be4a211bf02d3657b21900fc
David Shaw <david.shaw1242@gmail.com>
2024-09-17 05:37:16 -0400

    started working on split method

--- asteroid.py
+++ asteroid.py
@@ -1,4 +1,6 @@
+import random
 from circleshape import * 
+from constants import *
 
 class Asteroid(CircleShape):
     def __init__(self, x, y, radius):
@@ -9,3 +11,9 @@ class Asteroid(CircleShape):
 
     def update(self, dt):
         self.position += self.velocity * dt
+
+    def split(self):
+        self.kill()
+
+        if self.radius < ASTEROID_MIN_RADIUS:
+            return
